87442e3903337eb5dd526da0e6f863a1690d08eb
d4nmc <danielmcgee.work@gmail.com>
2021-07-16 11:11:34 +0100

    DM:building the crud in controller

--- src/main/java/com/natwest/springprojectmusic/controller/Controller.java
+++ src/main/java/com/natwest/springprojectmusic/controller/Controller.java
@@ -1,6 +1,13 @@
 package com.natwest.springprojectmusic.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +20,20 @@ public class Controller {
 		return "Back to testing on Postman";
 	}
 	
+	@PostMapping("/create")
+			public ResponseEntity<Music> create(){
+			return new ResponseEntity<Music>( body, HttpStatus.CREATED);
 	
+	}
+	
+	@GetMapping("/read")
+	public ResponseEntity<List<Music>> read() {
+		
+		return new ResponseEntity<List<Music>>( body, HttpStatus.OK);
+	}
+	
+	@PutMapping("/replace/{id")
+	public ResponseEntity<Music> update(@PathVariable Long id, @RequestBody Music ms) {
+		return new ResponseEntity<Music>( body, HttpStatus.ACCEPTED)
+	}
 }
