025dc3ff89ab6f1868a9a793ffabb8e570bce528
d4nmc <danielmcgee.work@gmail.com>
2021-07-16 10:56:33 +0100

    DM:starting controller and testing

--- src/main/java/com/natwest/springprojectmusic/controller/Controller.java
+++ src/main/java/com/natwest/springprojectmusic/controller/Controller.java
@@ -1,5 +1,17 @@
 package com.natwest.springprojectmusic.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/music")
 public class Controller {
 
+	@GetMapping("/test")
+	public String test() {
+		return "Back to testing on Postman";
+	}
+	
+	
 }
